7543ccb82263d88219750f624b6b06aef5253bf6
Matthew Pugh <pughmds@cyram.org>
2023-07-29 00:40:22 +0100

    Shift from terraform to cloudformation

--- infrastructure/modules/cloudwatch.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_cloudwatch_log_group" "dagster" {
-  name              = "${var.platform_name}-dagster"
-  retention_in_days = var.log_retention
-}
--- infrastructure/modules/output.tf
+++
@@ -1,3 +0,0 @@
-output "app_url" {
-  value = aws_alb.application_load_balancer.dns_name
-}
