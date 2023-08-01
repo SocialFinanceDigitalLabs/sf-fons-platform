6738d0e4396f027a98534ff3ca592eff7cf8af23
Matthew Pugh <pughmds@cyram.org>
2023-08-01 20:09:33 +0100

    Removed unneeded code

--- dagster/sync/repository.py
+++ dagster/sync/repository.py
@@ -1,5 +1,4 @@
 from dagster import repository
-from sync.jobs.liia import liia
 from sync.jobs.sync_code import sync_code
 
 
@@ -11,7 +10,7 @@ def sync():
     For hints on building your Dagster repository, see our documentation overview on Repositories:
     https://docs.dagster.io/overview/repositories-workspaces/repositories
     """
-    jobs = [sync_code, liia]
+    jobs = [sync_code]
     schedules = []
     sensors = []
 
